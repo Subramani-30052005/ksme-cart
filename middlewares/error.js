3997469e327cb0d95ade147f427730d3c87dbfaa
Subramani-30052005 <subramani30052005@gmail.com>
2025-12-04 22:50:23 +0530

    async promise error handler2

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,18 +1,40 @@
 module.exports =(err,req,res,next)=>{
     err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error";
 
     if(process.env.NODE_ENV == 'development'){
       res.status(err.statusCode).json({
         success:false,
         message: err.message,
-        stack : err.stack
+        stack : err.stack,
+        error:err
     })
 }
-    if(process.env.NODE_ENV == 'production'){
-        res.status(err.statusCode).json({
-        success:false,
-        message: err.message,
-        
-    })
+
+//production
+    if(process.env.NODE_ENV=='production'){
+        let message = err.message;
+        let error = new Error(message);
     }
-}
+    if (err.name === "ValidationError") {
+        const errors = Object.values(err.errors).map(e => e.message);
+        return res.status(400).json({
+            success: false,
+            message: errors
+        });
+    }
+
+    // CastError (Invalid ID)
+    if (err.name === "CastError") {
+        return res.status(400).json({
+            success: false,
+            message: `Invalid ${err.path}`
+        });
+    }
+
+    // Default error
+    return res.status(err.statusCode).json({
+        success: false,
+        message: err.message
+    });
+};
